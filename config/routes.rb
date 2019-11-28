6e3fa86c0479092117d4be516f13e38686d5893d
Uzayisenga <aminauzayisenga@gmail.com>
2019-11-28 17:43:22 +0200

    make it better

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
   resources :docteurs
   get 'welcome/index'
   root 'welcome#index'
+ 
   resources :doctors
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
